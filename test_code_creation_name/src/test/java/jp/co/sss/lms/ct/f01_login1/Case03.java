f63b3a1faf3a2e40fd128c5e57a5dce753affa69
mimaki.yusuke <mimaki.yusuke@192.168.0.9>
2026-09-25 11:53:42 +0900

    add:Case03.java コース詳細画面への遷移テストコード追加
    WebElementを用いて入力値を入力しログインボタンを押下し、コース詳細画面への遷移することを確認すテストを作成
     【入力値】
    ログインID：StudentAA01
    パスワード：StudentAA0
    
    add:Case04.java ヘルプ画面およびよくある質問画面への遷移テストコード追加
    ・WebElementを用いてヘルプリンクを取得しヘルプ画面に遷移するかを確認するテストの作成
    ・WebElementを用いてよくある質問リンクを取得しよくある質問画面に遷移するかを確認するテストの作成

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,11 +10,15 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.amazonaws.services.guardduty.model.Evidence;
 
 /**
  * 結合テスト ログイン機能①
  * ケース03
- * @author holy
+ * @author みまき
  */
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース03 受講生 ログイン 正常系")
@@ -35,14 +40,41 @@ public class Case03 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		webDriver.get("http://localhost:8080/lms");
+		//表示されているタイトルの確認
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		//URLの確認
+		assertEquals("http://localhost:8080/lms/", webDriver.getCurrentUrl());
+		//エビデンス取得
+		getEvidence(new Evidence() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//画面入力
+		WebElement idElement = webDriver.findElement(By.id("loginId"));
+		idElement.clear(); // 初期値をクリア
+		idElement.sendKeys("StudentAA01");
+
+		WebElement passElement = webDriver.findElement(By.id("password"));
+		passElement.clear(); // 初期値をクリア
+		passElement.sendKeys("StudentAA0");
+
+		WebElement loginElement = webDriver.findElement(By.className("btn-primary"));
+		loginElement.click();
+
+		//遷移後の画面確認
+		//表示されているタイトルの確認
+		visibilityTimeout(By.className("breadcrumb"), 10);
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+		//URLの確認
+		assertEquals("http://localhost:8080/lms/course/detail", webDriver.getCurrentUrl());
+		//エビデンス取得
+		getEvidence(new Evidence() {
+		});
 	}
 
 }
--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,11 +12,16 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.amazonaws.services.guardduty.model.Evidence;
 
 /**
  * 結合テスト よくある質問機能
  * ケース04
- * @author holy
+ * @author みまき
  */
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース04 よくある質問画面への遷移")
@@ -35,28 +43,91 @@ public class Case04 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		webDriver.get("http://localhost:8080/lms");
+		//表示されているタイトルの確認
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		//URLの確認
+		assertEquals("http://localhost:8080/lms/", webDriver.getCurrentUrl());
+		//エビデンス取得
+		getEvidence(new Evidence() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//画面入力
+		WebElement idElement = webDriver.findElement(By.id("loginId"));
+		idElement.clear(); // 初期値をクリア
+		idElement.sendKeys("StudentAA01");
+
+		WebElement passElement = webDriver.findElement(By.id("password"));
+		passElement.clear(); // 初期値をクリア
+		passElement.sendKeys("StudentAA0");
+
+		WebElement loginElement = webDriver.findElement(By.className("btn-primary"));
+		loginElement.click();
+
+		//遷移後の画面確認
+		//表示されているタイトルの確認
+		visibilityTimeout(By.className("breadcrumb"), 10);
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+		//URLの確認
+		assertEquals("http://localhost:8080/lms/course/detail", webDriver.getCurrentUrl());
+		//エビデンス取得
+		getEvidence(new Evidence() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		//機能ボタンをクリック
+		WebElement buttonlinkTextElement = webDriver.findElement(By.linkText("機能"));
+		buttonlinkTextElement.click();
+		pageLoadTimeout(10);
+		//ヘルプリンクをクリック
+		WebElement linkTextElement = webDriver.findElement(By.linkText("ヘルプ"));
+		linkTextElement.click();
+		//ヘルプ画面に遷移
+		visibilityTimeout(By.className("panel-heading"), 10);
+		assertEquals("ヘルプ | LMS", webDriver.getTitle());
+		//URLの確認
+		assertEquals("http://localhost:8080/lms/help", webDriver.getCurrentUrl());
+		//エビデンス取得
+		getEvidence(new Evidence() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+		// 現在のタブを保存
+		String originalWindow = webDriver.getWindowHandle();
+
+		WebElement linkTextElement = webDriver.findElement(By.linkText("よくある質問"));
+		linkTextElement.click();
+		//質問画面に遷移
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+		wait.until(driver -> driver.getWindowHandles().size() == 2);
+		// 新しいタブへ切り替える
+		for (String windowHandle : webDriver.getWindowHandles()) {
+			if (!windowHandle.equals(originalWindow)) {
+				webDriver.switchTo().window(windowHandle);
+				break;
+			}
+		}
+		visibilityTimeout(By.className("well"), 10);
+		assertEquals("よくある質問 | LMS", webDriver.getTitle());
+		//URLの確認
+		assertEquals("http://localhost:8080/lms/faq", webDriver.getCurrentUrl());
+		//エビデンス取得
+		getEvidence(new Evidence() {
+		});
+
 	}
 
 }
